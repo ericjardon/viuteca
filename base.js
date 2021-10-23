afb7e7463ed38bfe7e8fa3ab973ea80f1f8ec942
eric chao <ericjardon@hotmail.com>
2021-10-23 12:53:14 -0500

    Start work with groups controller file

--- base.js
+++ base.js
@@ -1,21 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { firebase } from "@firebase/app";
 import "@firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+// As per the Docs
+import { getFirestore } from "firebase/firestore";
+
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const app = initializeApp({
   apiKey: "AIzaSyBJNmbubofqnI1hko1awOhzBjQA5ooOsYw",
   authDomain: "viuteca-app.firebaseapp.com",
   projectId: "viuteca-app",
   storageBucket: "viuteca-app.appspot.com",
   messagingSenderId: "512178021982",
   appId: "1:512178021982:web:b8f6bbb7ed72d0a44d76cf"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export const app;
 export const auth = firebase.auth();
+export const db = getFirestore();
---
+++ src/api/groupsController.js
@@ -0,0 +1,87 @@
+import { db } from "../../base"
+
+const controller = {}
+
+controller.createGroup = async (groupId, group) => {
+    // use .add to auto generate an Id
+    const response = {
+        ok: null,
+        error: null,
+        data: null,
+    }
+
+    const ref = db.collection("groups").doc(groupId);
+    const doc_ = await ref.get()
+    // verify syntax: https://firebase.google.com/docs/firestore/query-data/get-data#web-version-8_1
+    if (doc_.exists) {
+        response.error = "El corre ya está registrado"
+        return response;
+    }
+
+    ref.set(
+        group
+    )
+    .then( newDoc => {
+        console.log("New group", group.name, "inserted succesfully");
+        response.ok = "New group " + group.name + " inserted succesfully"
+        response.data = newDoc
+        return response
+    })
+    .catch(err => {
+        console.log("Error:", err);
+        response.error = err;
+        return response;
+    })
+}
+
+controller.getGroups = () => {
+
+    const response = {
+        ok: null,
+        error: null,
+        data: null,
+    }
+
+    db.collection("groups").get()
+    .then(data => {
+            let groups = []
+
+            data.forEach(doc => {
+                console.log(`${doc.id} => ${doc.data()}`);
+                groups.push(doc.data())
+            })
+
+            response.ok = true;
+            response.data = groups;
+            return response;
+        }
+    )
+    .catch(err => {
+        response.error = err;
+        return response
+    })
+}
+
+controller.updateGroup = async (groupId, newData) => {
+    // use Set to use previous id
+    const response = {
+        ok: null,
+        error: null,
+    }
+
+    // .set does not return anything
+    try {
+        await db.collection("groups").doc(groupId)
+        .set(newData)
+        response.ok = true;
+        return response;
+
+    } catch (error) {
+        response.error = error;
+        return response;
+    }
+
+}
+
+
+export default controller;
