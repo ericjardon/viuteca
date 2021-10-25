49372cbdc19a10fd501ccb94e1bd78f48a13e717
unknown <a01026848@itesm.mx>
2021-10-24 22:54:21 -0500

    Searchbar, searchbar menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,38 @@
 import React, { useState } from 'react'
 import styles from './styles/NavBar.module.scss'
-import { Navbar, NavbarBrand, Nav, NavItem, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Input } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, Input, InputGroup, InputGroupAddon, InputGroupText, InputGroupButtonDropdown } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import photo from '../assets/viutecaLogoComplete.png'
 
 export default function NavHeader(props) {
     const [dropdownopen, setOpen] = useState(false);
+    const [splitButtonOpen, setSplitButtonOpen] = useState(false);
 
     const toggle = () => setOpen(!dropdownopen);
+    const toggleSplit = () => setSplitButtonOpen(!splitButtonOpen);    
 
     return (
-            <Navbar color="black" expand="lg">
-                <div className='container' style={{}}>
-                    <NavbarBrand className="mr-auto" href='/'>
-                        <img src={photo} alt='ViutecaLogo' />
+            <div>
+                <Navbar color="black">
+                    <NavbarBrand href='/' style={{width:"22%"}}>
+                            <img src={photo} alt='ViutecaLogo' />
                     </NavbarBrand>
-                    <Nav navbar>
-                        <NavItem >
-                            <Input></Input>
-                        </NavItem>
-                        <NavItem >
+                    <InputGroup style={{width:"45%"}}>
+                        {/* <InputGroupAddon><InputGroupText style={{backgroundColor:"white"}}><span>lupa</span></InputGroupText></InputGroupAddon>  */}
+                        <Input placeholder="Busqueda" ></Input>
+                        <InputGroupButtonDropdown isOpen={splitButtonOpen} toggle={toggleSplit}>
+                            <DropdownToggle split> </DropdownToggle>
+                            <DropdownMenu style={{backgroundColor: 'black', border:'None', fontSize:"100%"}}>
+                                <DropdownItem style={{color:'white'}}>Titulo</DropdownItem>
+                                <DropdownItem style={{color:'white'}}>Fecha</DropdownItem>
+                                <DropdownItem style={{color:'white'}}>Asosiación</DropdownItem>
+                            </DropdownMenu>
+                        </InputGroupButtonDropdown>
+                    </InputGroup>
+                    <Nav navbar style={{width:"22%"}}>
+                        <NavItem>
                             <ButtonDropdown  direction="down" isOpen={dropdownopen} toggle={toggle}>
-                                <DropdownToggle style={{backgroundColor: 'black', border:'None'}} caret>Menu</DropdownToggle>
+                                <DropdownToggle style={{backgroundColor: 'black', border:'None', fontSize:"170%"}} caret>Menu</DropdownToggle>
                                 <DropdownMenu  style={{backgroundColor: 'black'}}>
                                     <DropdownItem>
                                         <NavLink style={{color:'white'}} className='nav-link' to='/'>
@@ -46,8 +57,8 @@ export default function NavHeader(props) {
                             </ButtonDropdown>
                         </NavItem>
                     </Nav>
-                </div>
-            </Navbar >
+                </Navbar>
+            </div>
     )
 }
 
