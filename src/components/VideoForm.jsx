2f52b7fe1d2a5b5e886958d787a9f0ed5bcd1476
unknown <a01026848@itesm.mx>
2021-10-26 20:43:48 -0500

    Url, upload and publish button

--- src/components/VideoForm.jsx
+++ src/components/VideoForm.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Redirect } from 'react-router';
-import {auth} from '../base'
-import { Container, Row, Col, Label, Input, FormGroup, Form, CustomInput } from 'reactstrap';
+import { Container, Row, Col, Label, Input, FormGroup, Form, CustomInput, Button } from 'reactstrap';
 import controller from '../firebase/groups';
 
 /* Component for the main screen with listed videos */
@@ -12,17 +10,17 @@ export default function VideoForm() {
                 <Col xs='6'>
                     <Form style={{marginTop:"30px"}}>
                         <Row>
-                            <Label md={2}>Titulo</Label>
+                            <Label md={2} style={{textAlign:"left"}}>Titulo</Label>
                             <Col md={10}><Input placeholder="Introduce el título del video."></Input></Col>
                         </Row>
                         <Row style={{marginTop:"35px"}}>
-                            <Label md={2}>Duración</Label>
+                            <Label md={2} style={{textAlign:"left"}}>Duración</Label>
                             <Col md={3}><Input type="number" defaultValue="15"></Input></Col>
-                            <Label md={2}>Fecha</Label>
+                            <Label md={2} style={{textAlign:"left"}}>Fecha</Label>
                             <Col md={5}><Input type="date"></Input></Col>
                         </Row>
                         <Row style={{marginTop:"25px"}}>
-                            <Label md={2}>Descripción</Label>
+                            <Label md={2} style={{textAlign:"left"}}>Descripción</Label>
                             <Input type="textarea" ></Input>
                         </Row>
                     </Form>
@@ -35,14 +33,17 @@ export default function VideoForm() {
                         <Row>
                             <Label style={{textAlign:"left"}}>Sube tu video a drive e ingresa la liga en el siguiente espacio</Label>
                         </Row>
-                        <Row style={{marginTop:"25px"}}>
-                            <FormGroup>
-                                <CustomInput
-                                type="file"
-                                id="exampleCustomFileBrowser"
-                                name="customFile"
-                                label={'choose an image file'}/>
-                            </FormGroup>
+                        <Row style={{marginTop:"25px"}} >
+                            <Col md={10}><Input type="url" placeholder="Introduce el link."></Input></Col>
+                            <Col style={{marginLeft:"0px"}}><Button md={4} style={{backgroundColor:"#D9CAB3", color:"black"}}>Subir</Button></Col>
+                        </Row>
+                        <Row style={{marginTop:"20%"}}>
+                            <Label >El video fue cargado exitosamente</Label>
+                        </Row>
+                        <Row style={{marginTop:"5%"}}>
+                            <Col>
+                                <Button style={{backgroundColor:"#D9CAB3", color:"black", width:"22%"}}>Publicar</Button>
+                            </Col>
                         </Row>
                     </Form>
                 </Col>
