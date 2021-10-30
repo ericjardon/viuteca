2509aa513c2e5ebf096f6905265e1b7af474620f
eric chao <ericjardon@hotmail.com>
2021-10-30 18:23:25 -0500

    Hover effect and complete video card information, refactor responsive styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,36 @@ import styles from './styles/Card.module.scss';
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 function Card(props) {
-  const project = props.project;
+  const {project} = props;
+  const {data} =project;
+
+  const videoLength = (data.durationMins || '0') + ':' + String((data.durationSecs || '0')).padStart(2, '0')
+
   const divStyle = () => ({
-    backgroundImage: 'url(' + project.data.img + ')'
+    backgroundImage: 'url(' + data.img + ')'
   })
   return (
     <div className={styles.dummyContainer}>
       <div className={styles.Card}>
-        <div className={styles.CardImage} style={divStyle()} >
-        </div>
-        <p className={styles.paragraph}>
-          {project.data.description}
-        </p>
-        <Link to={"/video/" + project.id} style={{ backgroundColor: '#D9CAB3' }}>
-          <Button style={{ backgroundColor: '#D9CAB3' }}>Ver más</Button>
-        </Link>
 
+        <div className={styles.CardImage} style={divStyle()} >
+          <p className={styles.durationStamp}>
+            {videoLength}
+          </p>
+        </div>
+
+        <div className={styles.titleAndParagraph}>
+          <h4>{data.title}</h4>
+          <p className={styles.paragraph}>
+            {data.description}
+          </p>
+        </div>
+
+        <div className={styles.buttonContainer}>
+          <Link to={"/video/" + project.id} style={{ backgroundColor: '#D9CAB3' }}>
+            <Button style={{ backgroundColor: '#D9CAB3' }}>Ver más</Button>
+          </Link>
+        </div>
       </div>
     </div>
 
--- src/components/VideosDisplay.jsx
+++ src/components/VideosDisplay.jsx
@@ -3,32 +3,35 @@ import Card from './Card';
 import './styles/VideosDisplay.scss';
 import styles from './styles/VideoDetail.module.scss';
 import Video from '../firebase/videos'
-import { Spinner} from "reactstrap";
+import { Spinner } from "reactstrap";
 
 function VideosDisplay() {
   const [loading, setLoading] = useState(true);
   const [videos, setVideos] = useState({});
-  
+
   useEffect(() => {
     async function fetchData() {
-        const video = await Video.getAllVideos();
-        setVideos(video);
-        setLoading(false);
-        console.log(videos);
+      const video = await Video.getAllVideos();
+      setVideos(video);
+      setLoading(false);
+      console.log(videos);
     }
     fetchData();
   }, []);
   if (loading) return (
     <div className={styles.container}>
-        <Spinner children="" style={{ width: '15rem', height: '15rem' }} />
+      <Spinner children="" style={{ width: '15rem', height: '15rem' }} />
     </div>
   )
   return (
-    <div className="VideosDisplay">
-      {videos.map((project,index)=>(
-        <Card project={project} index={index}/>
-      ))} 
+    <div className="CenteredContainer">
+      <div className="VideosDisplay">
+        {videos.map((project, index) => (
+          <Card project={project} index={index} />
+        ))}
+      </div>
     </div>
+
   );
 }
 
