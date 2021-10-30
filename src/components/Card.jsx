0af556500cc5378f70d182758ff823cdba3503f8
Georgina <gina10111@hotmail.com>
2021-10-29 23:06:29 -0500

    correct css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,12 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 function Card(props) {
   const project = props.project;
+  const divStyle = () => ({
+    backgroundImage: 'url(' + project.data.img + ')'
+  })
   return (
     <div className="Card">
-      <div className="CardImage" style={{ backgroundImage: `url(project.data.img)` }} >
+      <div className="CardImage" style={divStyle()} >
       </div>
         <p className="paragraph">
             {project.data.description}
