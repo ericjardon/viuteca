a8153bcb60581f2458a0f46a1e91b00d0e8570f6
eric chao <ericjardon@hotmail.com>
2021-10-31 23:03:32 -0600

    Initial Home styles with three sections and copies. Not responsive yet

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import {Button} from 'reactstrap';
-import {auth} from '../base' 
+import { Button } from 'reactstrap';
+import { auth } from '../base'
 import styles from './styles/Home.module.scss';
+import illustration1 from '../assets/ill1-cuate.png';
+import illustration2 from '../assets/ill2-cuate.png';
+import illustration3 from '../assets/webinar-blue.png'
+import { BsArrowRight } from 'react-icons/bs'
+import { BiCheck } from 'react-icons/bi'
 
 /* Component for the main screen with listed videos */
+// <a href="https://storyset.com/web">Web illustrations by Storyset</a>
 export default function Home() {
     return (
         <div className={styles.HomeContainer}>
-            <h1>¡Bienvenidx a la Viuteca!</h1>
+            <div className={styles.firstSection}>
+                <div className={styles.textContainer1}>
+                    <p className={styles.HeadLine}>¡Bienvenidx a la Viuteca!</p>
 
-            <p className={styles.MainText}>Este es un proyecto desarrollado por estudiantes del Tec de Monterrey que 
-                busca poner a tu alcance las grabaciones de los eventos de grupos estudiantiles
-                para que no te pierdas de nada. <br/> Si formas parte de un grupo estudantil, ¡crea
-                tu cuenta para subir tus eventos! Así tendras más difusión dentro del Tec y podrás 
-                tener todas tus grabaciones en un solo lugar. 
-            </p>
+                    <p className={styles.MainText}>Viuteca es un proyecto desarrollado por estudiantes del Tec, que
+                        busca poner a tu alcance las grabaciones de los eventos de grupos estudiantiles
+                        para que no te pierdas de nada. <br /> Encuentra talleres, conferencias, presentaciones, congresos, sesiones informativas y más!
+                    </p>
+                    <Link to="/videos">
+                        <Button className={styles.StartButton} color="primary">Ir a Viuteca <BsArrowRight /></Button>
+                    </Link>
+                </div>
+                <div className={styles.imageContainer1}>
+                    <img className={styles.ill1} src={illustration1} alt="" />
+                </div>
 
-            <Link to="/videos">
-                <Button size="lg" color="primary">Explora Viuteca</Button>
-            </Link>
+            </div>
+            <div className={styles.secondSection}>
+                <div className={styles.imageContainer1}>
+                    <img className={styles.ill1} src={illustration2} alt="" />
+                    {/* <a href="https://storyset.com/online">Online illustrations by Storyset</a> */}
+                </div>
+                <div className={styles.textContainer1}>
+                    <p className={styles.HeadLine2}>Dale play a tus grupos estudiantiles</p>
 
+                    <p className={styles.MainText2}>
+                        La nueva realidad digital y el distanciamiento social han impulsado la creación de contenido
+                        multimedia por parte de los grupos estudiantiles más que nunca. <br/> <br/>
+                        ¿La principal ventaja? Podemos revisitar este contenido cuando queramos. <br/> 
+                        ¿El reto? Encontrarlo todo en un solo lugar, de forma
+                        ordenada y fácil de explorar. <br/> ¿La solución? ¡Viuteca!
+                    </p>
+                </div>
+
+            </div>
+            <div className={styles.thirdSection}>
+                <div className={styles.textContainer1}>
+                    <p className={styles.HeadLine3}>¿Eres de un grupo o sociedad estudiantil?</p>
+
+                    <p className={styles.MainText}>
+                        Si formas parte de un grupo estudantil, ¡crea
+                        tu cuenta para subir tus eventos! <br/> Tu grupo o sociedad tendrá más difusión y podrás
+                        tener todas tus grabaciones en un solo lugar. <br />
+                        En Viuteca, tus videos estarán a la vista de todos, donde pueden ser buscados, aplaudidos y compartidos.
+                    </p>
+                    <Link to="/videos">
+                        <Button className={styles.RegisterButton}>Regístrate <BiCheck/></Button>
+                    </Link>
+                </div>
+                <div className={styles.imageContainer1}>
+                    <img className={styles.ill1} src={illustration3} alt="" />
+                </div>
+            </div>
+            <div className={styles.attribution}><a href="https://storyset.com/web">Web illustrations by Storyset</a></div>
         </div>
+
     )
 }
