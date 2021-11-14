5d95296c8e0220c40bd78a0d0a3f3c8350d4c5cf
eric chao <ericjardon@hotmail.com>
2021-11-14 13:29:34 -0600

    fix upload video without image

--- src/components/VideoForm.jsx
+++ src/components/VideoForm.jsx
@@ -109,6 +109,8 @@ export default function VideoForm() {
                     console.log("Succesful image upload", imgUpload);
                     redirect()
                 }
+            } else {
+                redirect()
             }
         }
         setShowSpinner(false);
